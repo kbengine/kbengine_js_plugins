470f85dad915789d2bd7814fcea4c6147cf413ae
kbengine <liquidx@163.com>
2015-05-04 13:18:04 +0800

    up

--- kbengine.js
+++ kbengine.js
@@ -2283,7 +2283,7 @@ KBEngine.KBEngineApp = function(kbengineArgs)
 		this.serverScriptVersion = "";
 		this.serverProtocolMD5 = "";
 		this.serverEntityDefMD5 = "";
-		this.clientVersion = "0.5.0";
+		this.clientVersion = "0.6.0";
 		this.clientScriptVersion = "0.1.0";
 		
 		// player的相关信息
