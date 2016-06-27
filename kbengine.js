be8da02ae901815389e37929a33033a1cd4e5445
kbengine <3603661@qq.com>
2016-06-27 14:03:20 +0800

    v0.8.10

--- kbengine.js
+++ kbengine.js
@@ -2352,7 +2352,7 @@ KBEngine.KBEngineApp = function(kbengineArgs)
 		this.serverScriptVersion = "";
 		this.serverProtocolMD5 = "";
 		this.serverEntityDefMD5 = "";
-		this.clientVersion = "0.8.0";
+		this.clientVersion = "0.8.10";
 		this.clientScriptVersion = "0.1.0";
 		
 		// player的相关信息
