1df2f0f6a1b2f627fe6141f8addd4c43f5990d6f
zengmingnan <zengmingnan@bearjoy.com>
2019-05-13 10:54:17 +0800

    edit

--- kbengine_ts/kbengine.bat
+++ kbengine_ts/kbengine.bat
@@ -3,13 +3,13 @@ REM 首次使用会问是文件或文件夹,一律选文件
 REM 如果不是cocos creator,需要改下路径
 
 REM 这里需要设置kbengine_ts路径
-set kbengine=\
+set kbengine=%cd%\..\..\kbengine_js_plugins\kbengine_ts
 
 set libs=%cd%\assets\Libs
 if not exist %libs% (
     mkdir %libs%
 )
 
-xcopy /y /c /h /r %frame%\bin\kbengine.d.ts %cd%\kbengine.d.ts
+xcopy /y /c /h /r %kbengine%\bin\kbengine.d.ts %cd%\kbengine.d.ts
 
-xcopy /y /c /h /r %frame%\bin\kbengine.js %libs%\kbengine.js
+xcopy /y /c /h /r %kbengine%\bin\kbengine.js %libs%\kbengine.js
